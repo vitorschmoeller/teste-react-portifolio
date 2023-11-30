d3ac870520f28e4998a2c8113064ee9bf0796d77
Antonio Vitor <vitorschmoeller2000@gmail.com>
2023-11-30 16:24:28 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,4 @@
+import { FotoPerfil } from './styles'
+const Avatar = () => <FotoPerfil src="https://github.com/vitorschmoeller.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,8 +1,22 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/vitorschmoeller.png" />
-    <Titulo fontSize={20}>Vitor Schmoeller</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Vitor Schmoeller</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        vitorschmoeller
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
